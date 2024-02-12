6551f2274dd2e42adae9253543378cbeaa1a5c7f
MUSTAFA MAZIN AHMED <mustafa.hamedi.2001@gmail.com>
2024-02-13 00:38:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+// Code your solution in this fi,,
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+let welcomeMessage = `Welcome to ${companyName}, the most profitable company in ${mostProfitableNeighborhood}! Meet our CEO, ${companyCeo}.`;
+
+console.log('Company Name:', companyName);
+console.log('Most Profitable Neighborhood:', mostProfitableNeighborhood);
+console.log('Company CEO:', companyCeo);
+console.log('Welcome Message:', welcomeMessage);
+
+mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan Smith'; 
+
+console.log('Updated Most Profitable Neighborhood:', mostProfitableNeighborhood);
+console.log('Updated Company CEO:', companyCeo);
+
+
+
+
+
